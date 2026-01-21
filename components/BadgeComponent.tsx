3df7a94753d247a5ad5ef1963d74b75316915f8a
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2026-01-21 15:38:28 +0000

    ui: refactor product info into component and improve status badge clarity with icons

---
+++ components/BadgeComponent.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@mantine/core";
+import { statusIcon } from "./StatusIcons";
+
+const BadgeComponent = ({item}:Record<string, any>) => {
+  return (
+    <td>
+      {item.status.toLowerCase() == "active" ? (
+        <Badge leftSection={statusIcon.processing} variant="light">
+          {item.status}
+        </Badge>
+      ) : (
+        <Badge leftSection={statusIcon.completed} variant="light" color="green">
+          {item.status}
+        </Badge>
+      )}
+    </td>
+  );
+};
+
+export default BadgeComponent;
