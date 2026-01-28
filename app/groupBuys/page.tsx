546778c25d0f59ddd72066f43da74ab95dddf8d2
O'Neil Obidiaso <obidiasodumebi@gmail.com>
2026-01-28 19:13:11 +0000

    feat: update status synchronization to use filtered data

--- app/groupBuys/page.tsx
+++ app/groupBuys/page.tsx
@@ -1,17 +1,14 @@
 import TabComponent from "@/components/tab/TabComponent";
 import TitleHeader from "@/components/TitleHeader";
-import { groupBuyData } from "@/data/groupbuy";
 import { groupBuyCols } from "@/data/groupBuy_prod_and_users_columns";
 import Stats from "./Stats";
 import { getCampaignStats } from "./campaignStats";
 import { getGroupBuys } from "@/utils/fetchGroupBuys";
 const page = async () => {
   const activeData = await getGroupBuys("ACTIVE");
-  console.log("data here:", activeData);
+  const pastData = await getGroupBuys();
+  console.log("data here:", pastData);
 
-  const { totalAmount, activeCampaigns, failedCampaigns } = getCampaignStats(
-    activeData.results,
-  );
 
   const pageInfo = {
     title: "Group Buys",
@@ -23,17 +20,11 @@ const page = async () => {
         <h1 className="text-2xl font-semibold">{pageInfo.title}</h1>
         <p>{pageInfo.description}</p>
       </TitleHeader>
-      <Stats
-        totalAmount={totalAmount}
-        activeCampaigns={activeCampaigns}
-        failedCampaigns={failedCampaigns}
-      />
 
-      {/* <TabComponent products={groupBuyData} col={groupBuyCols} /> */}
       <TabComponent
         col={groupBuyCols}
         activeData={activeData.results}
-        pastData={activeData.results}
+        pastData={pastData.results}
       />
     </section>
   );
--- components/tab/TabComponent.tsx
+++ components/tab/TabComponent.tsx
@@ -7,6 +7,8 @@ import { TableColumnType } from "@/Types/tableColumnType";
 import Table from "../Layout/table/Table";
 import TableRows from "../Layout/table/TableRows";
 import GroupBuyRows from "@/app/groupBuys/GroupBuyRows";
+import Stats from "@/app/groupBuys/Stats";
+import { getCampaignStats } from "@/app/groupBuys/campaignStats";
 
 type TabComponentProps = {
   products?: GroupBuyType[];
@@ -20,35 +22,45 @@ const TabComponent = ({ activeData, pastData, col }: TabComponentProps) => {
     activeData || [],
   );
 
-  const tabValues = ["first", "second", "third"];
+  const tabValues = ["first", "second"];
 
   const handleTabChange = (value: string | null) => {
     if (!value) return;
 
     if (value === "second") {
       setFilteredProducts(pastData || []);
+    } else {
+      setFilteredProducts(activeData || []);
     }
-    setFilteredProducts(activeData || []);
   };
 
+  const { totalAmount, activeCampaigns, failedCampaigns } =
+    getCampaignStats(filteredProducts);
   return (
-    <Tabs defaultValue="first" onChange={handleTabChange}>
-      <Tabs.List>
-        <Tabs.Tab value="first">Active Orders</Tabs.Tab>
-        <Tabs.Tab value="second">Past Orders</Tabs.Tab>
-      </Tabs.List>
+    <section className="flex flex-col gap-12">
+      <Stats
+        totalAmount={totalAmount}
+        activeCampaigns={activeCampaigns}
+        failedCampaigns={failedCampaigns}
+      />
+      <Tabs defaultValue="first" onChange={handleTabChange}>
+        <Tabs.List>
+          <Tabs.Tab value="first">Active Orders</Tabs.Tab>
+          <Tabs.Tab value="second">Past Orders</Tabs.Tab>
+        </Tabs.List>
 
-      {tabValues.map((value) => (
-        <Tabs.Panel value={value} key={value}>
-          <div className="my-6 p-4 border border-dashed border-gray-300 shadow-md rounded-xl">
-            <Table columns={col || []}>
-              <TableRows data={filteredProducts} />
-              <GroupBuyRows data={filteredProducts} />
-            </Table>
-          </div>
-        </Tabs.Panel>
-      ))}
-    </Tabs>
+        {tabValues.map((value) => (
+          <Tabs.Panel value={value} key={value}>
+            <div className="my-6 p-4 border border-dashed border-gray-300 shadow-md rounded-xl">
+              <Table columns={col || []}>
+                <TableRows data={filteredProducts} />
+                <GroupBuyRows data={filteredProducts} />
+              </Table>
+            </div>
+          </Tabs.Panel>
+        ))}
+      </Tabs>
+    </section>
   );
 };
 
