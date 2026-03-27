3267322e66ed416c34e7b4dd41e5694ec1db8f06
godwin <your_mail>
2026-03-27 12:12:24 +0100

    Update redirect from root to login page in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/single_buys',
-        permanent: true,
-      },
-    ]
-  },
+    /* config options here */
+    async redirects() {
+        return [
+            {
+                source: "/",
+                destination: "/login",
+                permanent: false,
+            },
+        ];
+    },
 };
 
 export default nextConfig;
