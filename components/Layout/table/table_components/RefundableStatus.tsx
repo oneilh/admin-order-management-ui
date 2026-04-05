ecaf48640d737e74b39551253109ac0dc17606e0
godwin <your_mail>
2026-04-05 18:32:42 +0100

    feat: update RefundableStatus component to correct prop types and enhance rendering logic

--- components/Layout/table/table_components/RefundableStatus.tsx
+++ components/Layout/table/table_components/RefundableStatus.tsx
@@ -1,16 +1,20 @@
 type RefundableStatusProps = {
-  statusText: string;
-  allowRefund: string;
+    statusText: string | null;
+    allowRefund: boolean;
 };
 
 const RefundableStatus = ({
-  statusText,
-  allowRefund,
+    statusText,
+    allowRefund,
 }: RefundableStatusProps) => {
-  if (allowRefund) {
-    return <p className="text-neutral-500 text-sm">{statusText}</p>;
-  }
-  return <p className="text-neutral-500 text-sm">{statusText}</p>;
+    if (!statusText) return null; // nothing to show
+    return (
+        <p
+            className={`text-neutral-500 text-sm ${allowRefund ? "text-orange-500" : "text-neutral-500"}`}
+        >
+            {statusText}
+        </p>
+    );
 };
 
 export default RefundableStatus;
