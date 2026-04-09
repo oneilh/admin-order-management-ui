76b1f48c687c713eb58ae407470c760f77f1c712
godwin <your_mail>
2026-04-09 15:02:05 +0100

    feat: implement GroupBuyOrderDetailClient with data fetching and loading states

--- app/(dashboard)/group_buys/[id]/orders/[orderId]/GroupBuyOrderDetailClient.tsx
+++ app/(dashboard)/group_buys/[id]/orders/[orderId]/GroupBuyOrderDetailClient.tsx
@@ -1,14 +1,35 @@
+"use client";
 
+import { useQuery } from "@tanstack/react-query";
+import { fetchGroupBuyOrders } from "@/utils/fetchBuys";
 
 type Props = {
-	id: number
-	orderId: number
-}
+    id: number;
+    orderId: number;
+};
 
-const GroupBuyOrderDetailClient = ({id, orderId}: Props) => {
-  return (
-	<div>GroupBuyOrderDetailClient</div>
-  )
-}
+const GroupBuyOrderDetailClient = ({ id, orderId }: Props) => {
+    const {
+        data: group,
+        isLoading,
+        error,
+    } = useQuery({
+        queryKey: ["groupBuyOrder", id],
+        queryFn: () => fetchGroupBuyOrders(id),
+        select: (data) => data.results.find((s) => s.id === orderId),
+    });
 
-export default GroupBuyOrderDetailClient
+    if (isLoading) return <p>Loading...</p>;
+    if (error) return <p>Error loading group buy order details...</p>;
+    if (!group) return <p>Group buy order not found</p>;
+    return (
+        <div>
+            <h1 className="text-lg font-bold mb-4">
+                GroupBuy Order Detail Client
+            </h1>
+            <pre>{JSON.stringify(group, null, 2)}</pre>
+        </div>
+    );
+};
+
+export default GroupBuyOrderDetailClient;
