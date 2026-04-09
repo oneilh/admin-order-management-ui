665f08f9b4429e31cfdb23baf9b417f8c1f8d694
godwin <your_mail>
2026-04-09 14:34:58 +0100

    feat: create GroupBuyOrderDetailClient component with props

---
+++ app/(dashboard)/group_buys/[id]/orders/[orderId]/GroupBuyOrderDetailClient.tsx
@@ -0,0 +1,14 @@
+
+
+type Props = {
+	id: number
+	orderId: number
+}
+
+const GroupBuyOrderDetailClient = ({id, orderId}: Props) => {
+  return (
+	<div>GroupBuyOrderDetailClient</div>
+  )
+}
+
+export default GroupBuyOrderDetailClient
