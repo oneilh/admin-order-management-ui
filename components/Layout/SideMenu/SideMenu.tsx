b806fec847338c75cb22cbe83c0c4905e8665627
godwin <your_mail>
2026-04-13 10:58:41 +0100

    feat: add logout button to sidebar

--- components/Layout/SideMenu/SideMenu.tsx
+++ components/Layout/SideMenu/SideMenu.tsx
@@ -1,15 +1,18 @@
-import Avatar from "./Avatar";
 import NavLinks from "./NavLinks";
+import LogoutButton from "./LogoutButton";
 
 const SideMenu = () => {
-  return (
-    <section className="p-2 flex flex-col items-center justify-between bg-neutral-900 text-white">
-      <div className="p-2 flex flex-col gap-12 bg-neutral-900 text-white h-screen">
-        <h2>Grup</h2>
-        <NavLinks />
-      </div>
-    </section>
-  );
+    return (
+        <section className="p-2 flex flex-col items-center justify-between bg-neutral-900 text-white h-screen ">
+            <div className="p-2 flex flex-col gap-12 bg-neutral-900 text-white">
+                <h2>Grup</h2>
+                <NavLinks />
+            </div>
+            <div className="p-2">
+                <LogoutButton />
+            </div>
+        </section>
+    );
 };
 
 export default SideMenu;
